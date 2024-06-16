5a3f23d14bc172e0fd84006abacdd5baf64b0155
Ahsan Ahmed <162038567+AhsXan@users.noreply.github.com>
2024-06-16 13:39:54 +0000

    OK

--- password_genarator/src/App.jsx
+++ password_genarator/src/App.jsx
@@ -9,26 +9,52 @@ function App() {
   const [Pass_allowed, setPass_allowed]=useState("")
   
   const passwordGenarator=useCallback(()=>{
-  let pass= ""
-  let char="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-  
-  if(num_allowed) char+="0123456789";
-  if(char_allowed) char+="!@#$%^&*()";
+  let word=""
+  let t_char= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-  for(let i=0;i<len;i++){
-    let index =Math.floor(Math.random() * arr.length+1);
-    pass= char.charAt(index)
-  } 
+  if (num_allowed) t_char += "1234567890"
+  if (char_allowed) t_char += "!@#$%&*(){}:<>?,./;'[]</>"
   
-  setPass_allowed(pass)
+  for (let i=0;i<len;i++){
+    let index= Math.floor(Math.random()*len.t_char+1);
+
+    word= t_char.charAt(index);
+
+  }
+
+setPass_allowed(word);
+
+
+
+
+
+    // let pass= ""
+  // let char="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  
+  // if(num_allowed) char+="0123456789";
+  // if(char_allowed) char+="!@#$%^&*()";
+
+  // for(let i=0;i<len;i++){
+  //   let index =Math.floor(Math.random() * arr.length+1);
+  //   pass= char.charAt(index)
+  // } 
+  
+  // setPass_allowed(pass)
 }
   ,[len,num_allowed,char_allowed,Pass_allowed]);
 
   return (
     <> 
         <h1 className='text-white text-center text-4xl py-14'>Password Genarator</h1>
-        <div className='w-full max-w-md '>
+        <div className='text-white w-full max-w-md  mx-auto shadow-md rounded-lg overflow-hidden md-4'>
+        Pass:  <input
 
+        type="text" 
+        className="" 
+        placeholder='Password'
+        value={Pass_allowed}
+        readOnly
+        />
 
 
         </div>
